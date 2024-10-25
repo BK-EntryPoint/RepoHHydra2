134d91c533f1452a76295c7938b7d595e5fce1e0
DESKTOP-4L63IVJ\Boris <boris.kunda@entrypoint.org.il>
2024-10-25 17:18:39 +0300

    initial commit

---
+++ app/src/main/java/com/example/hydrarepo2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.hydrarepo2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String vcsIndicator = "master_branch";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
